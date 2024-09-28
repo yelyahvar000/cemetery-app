daab4e055d0a7fb8d447d9aae73e2d42687101a1
Raquel028 <raquel.tugagao@gmail.com>
2024-09-29 01:12:35 +0800

    Text field and button

--- src/feature/registration-page/index.js
+++ src/feature/registration-page/index.js
@@ -1,9 +1,35 @@
-import { Button } from "@mui/material"
+import { Button, TextField } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+
 
 export const RegistrationPage = () => {
-    return <div > kendz <
-        Button variant = "text" > Text < /Button> <
-        Button variant = "contained" > Contained < /Button> <
-        Button variant = "outlined" > Outlined < /Button> <
-        /div>;
+  return (
+    <div>
+      kendz
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+         <TextField id="outlined-basic" label="First Name" variant="outlined" />
+        <Grid item xs={4}>
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+        </Grid>
+        <Grid item xs={8}>
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </Grid>
+        <Grid item xs={8}>
+         <Button variant="outlined">Sign Up</Button>
+          </Grid>
+      </Grid>
+      </Grid>
+      
+      <TextField
+        id="outlined-basic"
+        label="Re-enter Password"
+        variant="outlined"
+      />
+      
+    </div>
+  );
 };
