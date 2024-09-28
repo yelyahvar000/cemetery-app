39e1e400a1f7c6c4c9cdaa6794a061e501858dd6
Raquel028 <raquel.tugagao@gmail.com>
2024-09-29 00:34:56 +0800

    add MUI

--- src/feature/registration-page/index.js
+++ src/feature/registration-page/index.js
@@ -1,3 +1,9 @@
+import { Button } from "@mui/material"
+
 export const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+    return <div > kendz <
+        Button variant = "text" > Text < /Button> <
+        Button variant = "contained" > Contained < /Button> <
+        Button variant = "outlined" > Outlined < /Button> <
+        /div>;
 };
--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./store";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    Provider store = { store } >
+    <
+    App / >
+    <
+    /Provider> <
+    /React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
